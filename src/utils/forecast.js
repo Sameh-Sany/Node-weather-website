dd1446aae9c5bd42e52619adb312e3856e868ab3
Dev-Sameh <92803501+Sameh-Sany@users.noreply.github.com>
2022-03-18 12:56:11 +0200

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,26 @@
+const request = require("request");
+const forecast = (latitude, longitude, callback) => {
+  const url =
+    "http://api.weatherstack.com/current?access_key=34991b317d3cb91f22d9b99662c92cc8&query=" +
+    latitude +
+    "," +
+    longitude;
+
+  request({ url, json: true }, (error, { body }) => {
+    if (error) {
+      callback("unable to connect weather stack service", undefined);
+    } else if (body.error) {
+      callback("unable to find location", undefined);
+    } else {
+      callback(
+        undefined,
+        "its currently like  " +
+          body.current.temperature +
+          "  degree out.its feel like  " +
+          body.current.feelslike +
+          " degree out"
+      );
+    }
+  });
+};
+module.exports = forecast;
